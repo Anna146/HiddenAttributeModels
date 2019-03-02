639d97bd5a50499a5ea58672d747faef92b596e2
Anna <belpku001@mail.ru>
2019-03-02 18:59:19 +0100

    Create clean_input_msg.py

---
+++ prepare_data/clean_input_msg.py
@@ -0,0 +1,27 @@
+import re
+
+input = ""
+output = ""
+
+with open(input, "r") as f_in, open(output, "w") as f_out:
+    for line in f_in:
+        line = line.strip().split("\t")
+        txt = line[-1].lower()
+
+        pattern = re.compile('#[\w]*')
+        txt = pattern.sub(' ', txt)
+        pattern = re.compile('[\w]*@[\w]*')
+        txt = pattern.sub(' ', txt)
+        txt = re.sub(r'https?:\/\/.*[\r\n]*', '', txt)
+
+        pattern = re.compile('([^\s\w\']|_)+|\d|\t|\n')
+        txt = pattern.sub(' ', txt)
+        pattern = re.compile('\s+')
+        txt = pattern.sub(" ", txt)
+        pattern = re.compile('\.+')
+        txt = pattern.sub(".", txt)
+
+        post_len = len([x for x in txt.split(" ") if len(x.strip().strip(".")) > 1])
+        if post_len > 100 or post_len < 10:
+            continue
+        f_out.write("%s\t%s\t%s\n" % (line[0], line[1], txt))
